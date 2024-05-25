f16d1edfb8407a56660e873435daadbf2f0955b9
Srinu <srinumaka9908@gmail.com>
2024-05-25 15:55:46 +0530

    my first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly("srinu");
+        System.out.println(obj.getName());
+        obj.setName("srinu1");
+        System.out.println(obj.getName());
+    }
   
 }
